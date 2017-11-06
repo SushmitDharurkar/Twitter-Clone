13743c68974875b63a26d4a3e43f5ba7a63dd3c7
SushmitD <dharurkarsushmit@gmail.com>
2017-11-06 11:45:33 -0500

    added notifications field to engine state for tweets from users following

--- project4/lib/project4.ex
+++ project4/lib/project4.ex
@@ -5,40 +5,24 @@ defmodule Project4 do
     {_, server} = Client.start_link("")
     # IO.inspect(server)
 
-    #TODO Add recursion for continuos listening
-    user_pid1 = spawn(fn ->
-      #Receiving tweet from user following
-      receive do
-        {:tweet, tweet} -> IO.inspect(tweet)
-      end
-    end)
+    user_pid1 = spawn(Project4, :listen_for_tweets, [])
     # IO.inspect()
     Client.add_user(server, user_pid1)
 
-    user_pid2 = spawn(fn ->
-      #Receiving tweet from user following
-      receive do
-        {:tweet, tweet} -> IO.inspect(tweet)
-      end
-    end)
+    user_pid2 = spawn(Project4, :listen_for_tweets, [])
     # IO.inspect()
     Client.add_user(server, user_pid2)
 
     # IO.inspect()
     Client.add_follower(server, user_pid1, user_pid2)
 
-    user_pid3 = spawn(fn ->
-      #Receiving tweet from user following
-      receive do
-        {:tweet, tweet} -> IO.inspect(tweet)
-      end
-     end)
+    user_pid3 = spawn(Project4, :listen_for_tweets, [])
     # IO.inspect()
     Client.add_user(server, user_pid3)
 
     Client.add_follower(server, user_pid1, user_pid3)
 
-    Client.add_follower(server, user_pid2, user_pid1)
+    Client.add_follower(server, user_pid2, user_pid3)
 
     IO.inspect(Client.get_users(server), label: "Users: ")
 
@@ -48,10 +32,21 @@ defmodule Project4 do
 
     Client.send_tweet(server, user_pid2, "Second tweet!!")
 
+    Client.send_tweet(server, user_pid1, "Third tweet!!")
+
     receive do
       msg -> IO.puts(msg)
     end
 
+  end
+
+  #NOTE Elixir does not have nested functions
+  def listen_for_tweets() do
+    #Receiving tweet from user following
+    receive do
+      {:tweet, tweet} -> IO.inspect(tweet)
+    end
+    listen_for_tweets()
   end
 
 end
--- project4/lib/server.ex
+++ project4/lib/server.ex
@@ -2,21 +2,15 @@ defmodule Server do
     use GenServer
 
     def init(:ok) do
-        #{:ok, %{"users" => [pid1, pid2..], "user_details" => %{pid => %{"tweets" => [], "followers" =>[]}}}}
+        # {:ok, %{"users" => [pid1, pid2..], "hashtags" => %{}, "user_details" => %{pid => %{"tweets" => [], "followers" =>[], "notifications" => %{source_pid => [tweet1, ...]}}}}}
         {:ok, %{"users" => [], "user_details" => %{} }}
     end
 
     def handle_cast({:add_user, user_pid}, state) do
-      #NOTE Could change this if statement
-        if Map.get(state, "users") != nil do
-            user_list = [user_pid | Map.get(state, "users")]
-            state = Map.put(state, "users", user_list)
-          else
-            state = Map.put(state, "users", [user_pid])
-        end
-
+        users = state["users"]
+        state = Map.put(state, "users", [user_pid | users])
         user_details = Map.get(state, "user_details")
-        user_details = Map.put(user_details, user_pid, %{"tweets" => [], "followers" => []})
+        user_details = Map.put(user_details, user_pid, %{"tweets" => [], "followers" => [], "notifications" => %{}})
         state = Map.put(state, "user_details", user_details)
 
         #  IO.inspect(state, label: "State after adding user: ")
@@ -25,7 +19,6 @@ defmodule Server do
 
     def handle_cast({:add_follower, user_pid, follower_pid}, state) do
         #TODO Add checks if user doesn't exist
-
         user_details_pid = state["user_details"][user_pid]
         followers = user_details_pid["followers"]
         user_details_pid = Map.put(user_details_pid, "followers", [follower_pid | followers])
@@ -46,10 +39,21 @@ defmodule Server do
         state = Map.put(state, "user_details", user_details)
 
         followers = state["user_details"][user_pid]["followers"]
-        #Sending tweet to every follower
-        #NOTE Forwarded tweet not added in follower's state, just displayed
-        Enum.each(followers, fn(follower_pid) ->
+        #Sending tweet to every follower and adding it to their notifications
+        #NOTE Outside scope of state not accessed inside Enum - Use recursion or Enum.reduce()
+        state = Enum.reduce(followers, state, fn(follower_pid, state) ->
+          user_details_pid = state["user_details"][follower_pid]
+          notifications = user_details_pid["notifications"]
+          if notifications[user_pid] != nil do
+              notifications = Map.put(notifications, user_pid, [tweet | notifications[user_pid]])
+            else
+              notifications = Map.put(notifications, user_pid, [tweet])
+          end
+          user_details_pid = Map.put(user_details_pid, "notifications", notifications)
+          user_details = state["user_details"]
+          user_details = Map.put(user_details, follower_pid, user_details_pid)
           send(follower_pid, {:tweet, [tweet] ++ ["-Tweet from user: "] ++ [user_pid] ++ ["forwarded to follower: "] ++ [follower_pid] })
+          state = Map.put(state, "user_details", user_details)
          end)
 
         IO.inspect(state, label: "State after sending a tweet: ")
